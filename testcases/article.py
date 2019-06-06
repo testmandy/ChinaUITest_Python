d1736a7b1933b563d6bd65c93e8e73219e47681c
mandy <mandy.wan@dingtone.me>
2019-06-06 12:06:13 +0800

    fix issue ModuleNotFoundError: No module named

--- testcases/article.py
+++ testcases/article.py
@@ -10,7 +10,7 @@ from utils.get_by_axis import GetByAxis
 
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
-sys.path.append(rootPath)
+sys.path.append(os.path.split(rootPath)[0])
 
 
 class Article:
